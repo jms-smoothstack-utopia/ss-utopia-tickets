1fd110705d0eba80626ddb8ba57c52b321b8f381
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-02 18:21:49 -0500

    Clean dependency injection

--- src/main/java/com/ss/utopia/tickets/controller/EndpointConstants.java
+++ src/main/java/com/ss/utopia/tickets/controller/EndpointConstants.java
@@ -1,5 +1,5 @@
 package com.ss.utopia.tickets.controller;
 
 public class EndpointConstants {
-  public static final String TICKETS_ENDPOINT = "/api/v0.1/tickets";
+  public static final String API_V_0_1_TICKETS = "/api/v0.1/tickets";
 }
