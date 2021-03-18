abac1e65e2a871280d41259106a2b4bda9d3fb21
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-17 22:33:13 -0400

    Configure eureka for ECS

--- src/main/java/com/ss/utopia/tickets/TicketsApplication.java
+++ src/main/java/com/ss/utopia/tickets/TicketsApplication.java
@@ -1,7 +1,13 @@
 package com.ss.utopia.tickets;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class TicketsApplication {
@@ -10,4 +16,14 @@ public class TicketsApplication {
     SpringApplication.run(TicketsApplication.class, args);
   }
 
+  @Profile("ecs")
+  @Bean
+  public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils)
+      throws UnknownHostException {
+    var config = new EurekaInstanceConfigBean(inetUtils);
+    config.setIpAddress(InetAddress.getLocalHost().getHostAddress());
+    config.setNonSecurePort(8082);
+    config.setPreferIpAddress(true);
+    return config;
+  }
 }
