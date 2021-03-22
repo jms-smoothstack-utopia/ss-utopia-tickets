1016bb30441b87fe30c5a40a9ff143e47999e20e
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-22 15:36:52 -0400

    hotfix to make Checkstyle happy

--- src/main/java/com/ss/utopia/tickets/dto/TicketItem.java
+++ src/main/java/com/ss/utopia/tickets/dto/TicketItem.java
@@ -1,12 +1,11 @@
 package com.ss.utopia.tickets.dto;
 
+import java.time.ZonedDateTime;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.ZonedDateTime;
 
 @Data
 @NoArgsConstructor
